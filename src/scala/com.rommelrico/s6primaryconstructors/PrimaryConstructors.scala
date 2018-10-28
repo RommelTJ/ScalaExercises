29d4f540d6dc8ad27bb72448c0de20fa3a980e34
Rommel Rico <me@rommelrico.com>
2018-10-28 10:30:32 -0700

    Adding a primary constructor inline

--- src/scala/com.rommelrico/s6primaryconstructors/PrimaryConstructors.scala
+++ src/scala/com.rommelrico/s6primaryconstructors/PrimaryConstructors.scala
@@ -3,11 +3,11 @@ package com.rommelrico.s6primaryconstructors
 object PrimaryConstructors extends App {
 
   println("Start")
-  val t = new StringHolder()
+  val t = new StringHolder("This is a test")
   println("End")
 
 }
 
-class StringHolder {
-  println("Creating object instance")
+class StringHolder(text: String) {
+  println(s"Holding string of ${text.length} chars")
 }
