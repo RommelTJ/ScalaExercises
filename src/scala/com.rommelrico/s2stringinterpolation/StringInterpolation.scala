dfec170e8e2375563535e5c4f7de3c6bc9e113f2
Rommel Rico <me@rommelrico.com>
2018-10-28 10:11:04 -0700

    Different kinds of string interpolation

--- src/scala/com.rommelrico/s2stringinterpolation/StringInterpolation.scala
+++ src/scala/com.rommelrico/s2stringinterpolation/StringInterpolation.scala
@@ -4,6 +4,8 @@ object StringInterpolation extends App {
 
   val person = Person("Rommel", 30)
   println(s"Hello ${person.name}, how are you?")
+  println(s"You are ${person.age} years old")
+  println("You are " + person.age + " years old")
 
 }
 
