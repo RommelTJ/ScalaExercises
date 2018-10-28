b811a4a0b84ae7d3affba310875ebfb85cd282bf
Rommel Rico <me@rommelrico.com>
2018-10-28 10:09:45 -0700

    String interpolation with accessing object properties

--- src/scala/com.rommelrico/s2stringinterpolation/StringInterpolation.scala
+++ src/scala/com.rommelrico/s2stringinterpolation/StringInterpolation.scala
@@ -1,5 +1,10 @@
 package com.rommelrico.s2stringinterpolation
 
-class StringInterpolation {
+object StringInterpolation extends App {
+
+  val person = Person("Rommel", 30)
+  println(s"Hello ${person.name}, how are you?")
 
 }
+
+case class Person(name: String, age: Int)
